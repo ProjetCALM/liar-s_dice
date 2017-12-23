f9d06ae185524e26753b09dcae429a6953771dd8
Jrbdxa <34715106+Jrbdxa@users.noreply.github.com>
2017-12-23 15:21:15 +0100

    Update fonctionsliar.c

--- fonctionsliar.c
+++ fonctionsliar.c
@@ -7,6 +7,26 @@
 #include "headerliar.h"
 
 
+void liarDice(joueur rep[]) {
+    int p=0;
+    srand(time(0));
+    system("cls");
+    printf("**** LIAR'S DICE ****\n\nEntrer nombre de participants : ");
+    scanf("%d", &p);
+    
+    player table[p]; //crée un tableau contenant toutes les données de tous les joueurs de la table
+    initTable(table, rep, p);
+    printf("Initialise");
+    turn(table, p);
+}
+
+
+
+
+
+
+
+
 void letTheGameBegin(player table[]) {
     
     
@@ -15,40 +35,41 @@ void letTheGameBegin(player table[]) {
 
 
 
-void turn(player a/*table[], int p*/) {
+void turn(player table[], int p) {
     int rnd=1, end=0, i=0; //rnd permet de suivre le nombre de tours, end permet de mettre fin à la partie lorsqu'un joueur a gagné
     char enter=0; //utile pour certaines confirmations
     for(rnd=1; end != 6; rnd++) {
-        system("cls");
-        //for(i=0; i<p; i++) { //pour faire le tour de la table en fonction du nombre de participants
-            printf("Joueur actuel : %s\n", a/*table[]*/.nom);
-            printf("Jetons : %d\n\n", a/*table[]*/.bet);
+        for(i=0; i<p; i++) { //pour faire le tour de la table en fonction du nombre de participants
+            system("cls");
+            printf("Joueur actuel : %s\n", table[i].nom);
+            printf("Jetons : %d\n\n", table[i].bet);
         
             if(rnd == 1) {
-              printf("ROLL THE DICE !\n");
-              while (enter != '\r' && enter != '\n') { enter = getchar(); }
-               enter=0;
-               a/*table[p]*/=rollTheDice(a/*table[p)*/);
+                printf("ROLL THE DICE !\n");
+                fflush(stdin);
+                while (enter != '\r' && enter != '\n') { enter = getchar(); }
+                enter=0;
+                table[i]=rollTheDice(table[i]);
             }
         
             else {
                 printf("Des actuels :\n");
-                printTheDice(a/*table[p)*/);
+                printTheDice(table[i]);
             }
             end=rnd*2;
             locate(0, 12);
             printf("Press [Enter] to begin next turn.");
             while (enter != '\r' && enter != '\n') { enter = getchar(); }
             enter=0;
-        //}
+        }
     }
         
     
 }
 
-void initTable(player table[], joueur rep[], int n) {
+void initTable(player table[], joueur rep[], int p) {
     int i=0, j=0;
-    for (i=0; i<n; i++) {
+    for (i=0; i<p; i++) {
         table[i].score=rep[i].score;
         strcpy(table[i].nom, rep[i].nom);
         for(j=0; j<5; j++) {
@@ -56,6 +77,25 @@ void initTable(player table[], joueur rep[], int n) {
         }
         table[i].bet=100;
     }
+}
+
+player rollTheDice(player a) {
+    int i=0;
+    for(i=0; i<5; i++) { 
+        a.dice[i]=(rand()%6)+1; //calcule un résultat entre 1 et 6 pour le dé
+    }
+    printTheDice(a);
+    return a;
+} //note : pas certain que le fonctionnement des pointeurs permette de récupérer le résultat des dés. A vérifier.
+ 
+void locate(int x,int y) { 
+    //permet de replacer le curseur à l'endroit désiré, afin que votre prochain printf se fasse à l'endroit que vous souhaitez
+    //fonctionne grâce à windows.h, pensez à l'inclure. Ne marchera probablement pas pour toi, Adrien.
+    HANDLE H=GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD C;
+    C.X=x;
+    C.Y=y;
+    SetConsoleCursorPosition(H,C);
 }
 
 void printTheDice(player a) { //permet de lancer et d'afficher les 6 dés
@@ -134,23 +174,4 @@ void printTheDice(player a) { //permet de lancer et d'afficher les 6 dés
             printf("+---------+\n");
         }
     }
-}
-
-player rollTheDice(player a) {
-    int i=0;
-    for(i=0; i<5; i++) { 
-        a.dice[i]=(rand()%6)+1; //calcule un résultat entre 1 et 6 pour le dé
-    }
-    printTheDice(a);
-    return a;
-} //note : pas certain que le fonctionnement des pointeurs permette de récupérer le résultat des dés. A vérifier.
- 
-void locate(int x,int y) { 
-    //permet de replacer le curseur à l'endroit désiré, afin que votre prochain printf se fasse à l'endroit que vous souhaitez
-    //fonctionne grâce à windows.h, pensez à l'inclure. Ne marchera probablement pas pour toi, Adrien.
-    HANDLE H=GetStdHandle(STD_OUTPUT_HANDLE);
-    COORD C;
-    C.X=x;
-    C.Y=y;
-    SetConsoleCursorPosition(H,C);
 }
