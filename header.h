2e02ccdd904addc49334b25c0dac0a14325f49c3
Jrbdxa <34715106+Jrbdxa@users.noreply.github.com>
2017-12-23 02:24:55 +0100

    Update header.h

--- header.h
+++ header.h
@@ -1,1 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
+/* 
+ * File:   header.h
+ * Author: prico
+ *
+ * Created on 20 décembre 2017, 16:55
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <windows.h> //permet d'utiliser la fonction locate
+
+#define NOM ((int)100) //taille max du nom de joueur
+#define REP ((int)50) //taille max du répertoire de joueurs
+
+typedef struct Joueur
+{
+    char nom[NOM];
+    int score;
+}joueur;
+
+typedef struct Player
+{   //crée une structure similaire à Joueur, en ajoutant un tableau prenant en compte les 5 dés, ainsi qu'une valeur pour le pari
+    char nom[NOM];
+    int score;
+    int dice[5];
+    int bet;
+}player;
+
+void letTheGameBegin(player table[]);
+void turn(player /*table[], int*/);
+void initTable(player table[], joueur rep[], int);
+player rollTheDice(player);
+void printTheDice(player);
+void locate(int, int);
